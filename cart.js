7bdf4df98a31126e40423a0f261a6d57db26cb90
mo <140063452+M4moon@users.noreply.github.com>
2023-07-20 15:03:28 +0500

    Menu Uploaded
    added my menu page project on github

---
+++ cart.js
@@ -0,0 +1,39 @@
+// Initialize an empty cart array to store cart items
+let cart = [];
+
+
+function toggleCartDropdown() {
+    const cartDropdown = document.getElementById('cartDropdown');
+    cartDropdown.style.display = cartDropdown.style.display === 'none' ? 'block' : 'none';
+}
+
+function addToCart(itemName, itemPrice) {
+    // Create an object representing the cart item
+    const item = {
+        name: itemName,
+        price: itemPrice
+    };
+
+    // Add the item to the cart array
+    cart.push(item);
+
+    // Update the cart display
+    updateCartDisplay();
+}
+
+function updateCartDisplay() {
+    const cartElement = document.getElementById('cart');
+    const cartCountElement = document.getElementById('cartCount');
+    cartCountElement.textContent = cart.length.toString();
+
+    // Clear the current cart display
+    cartElement.innerHTML = '';
+
+    // Loop through the cart items and create list elements to display them
+    cart.forEach(item => {
+        const listItem = document.createElement('li');
+        listItem.textContent = `${item.name} - Rs. ${item.price}`;
+        cartElement.appendChild(listItem);
+    });
+}
+
